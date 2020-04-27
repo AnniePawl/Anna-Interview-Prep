35f35f3c78e96227e300e7379667f15a25f2e4ad
Anna Pawl <annampawl@gmail.com>
2020-04-26 22:39:19 -0400

    another solution for max_subarray

--- SPD2.4/max_subarray.py
+++ SPD2.4/max_subarray.py
@@ -29,3 +29,19 @@ def contiguous_sequence(nums): # Time: O(n), Space: O(1)
 print(contiguous_sequence([2, -8, 3, -2, 4, -10])) # 5
 print(contiguous_sequence([2, -8, 5, -2, 4, -10])) # 7
 
+
+def max_subarray(a, size):
+    max_so_far = -1000 # max int
+    max_ending_here = 0 # current sum 
+
+    for i in range(0, size):
+        max_ending_here = max_ending_here + a[i]
+        if (max_so_far < max_ending_here):
+            max_so_far = max_ending_here
+        
+        if max_ending_here <0:
+            max_ending_here = 0 
+    return max_so_far
+
+a= [2, -8, 3, -2, 4, -10]
+print(max_subarray(a ,len(a)))
