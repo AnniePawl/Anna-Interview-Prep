5d2b4b9eb247378ae3ab34e96edf4025e7dd5d86
Anna Pawl <annampawl@gmail.com>
2020-05-26 14:48:23 -0400

    min sum array

--- CodeWars/7kyu/arrays/array_series/min_sum_of_arrays.py
+++ CodeWars/7kyu/arrays/array_series/min_sum_of_arrays.py
@@ -1,16 +1,30 @@
-# Given array of ints, find min nsum which can be obtained from summing each to integers product 
+# Given array of ints, find min sum which can be obtained from summing each two integers product
 
 def min_sum(arr):
-  sorted_nums = sorted(arr)
+  sum = 0
+  sorted_arr = sorted(arr)
   front_i = 0 
-  back_i = 0
-  for i in sorted_nums:
-    
-
+  back_i = len(arr)-1
   
+  while front_i < back_i:
+    sum += sorted_arr[front_i] * sorted_arr[back_i]
+    front_i +=1
+    back_i -=1
+  return sum
+
+
+def min_sum2(arr):
+  arr = sorted(arr)
+  return sum(arr[i]* arr[-i-1] for i in range(len(arr)//2))
+
 
 
 
 print(min_sum([5,4,2,3])) # 22 -> 5*2 + 3*4
 # [3,2,4,5]
 print(min_sum([9,2,8,7,5,4,0,6])) # 74
+
+
+print(min_sum2([5,4,2,3])) # 22 -> 5*2 + 3*4
+# [3,2,4,5]
+print(min_sum2([9,2,8,7,5,4,0,6])) # 74
