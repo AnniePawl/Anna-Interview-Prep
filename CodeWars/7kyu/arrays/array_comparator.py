974b88d031be49032057e375584095d7743c3773
Anna Pawl <annampawl@gmail.com>
2020-05-26 19:13:39 -0400

    codewars 7kyu

---
+++ CodeWars/7kyu/arrays/array_comparator.py
@@ -0,0 +1,20 @@
+# Given 2 arrays with unique elements, return number of elements that are in both 
+
+
+def array_comparator(a,b):
+  set1 = set(a)
+  set2 = set(b)
+  return len(set1.intersection(set2))
+
+def array_comparator2(a,b):
+  return len(set(a).intersection(set(b)))
+
+def array_comparator3(a,b):
+  return sum(x in b for x in a)
+
+print(array_comparator(['button', 5, True, 'gossip'], [False, 'gossip', 10, 'jumbo', 5])) # 2 (5 and 'gossip)
+
+
+print(array_comparator2(['button', 5, True, 'gossip', False], [False, 'gossip', 10, 'jumbo', 5])) # 3 (5 and 'gossip, False)
+
+print(array_comparator3(['button', 5, True, 'gossip', False], [False, 'gossip', 10, 'jumbo', 5])) # 3 (5 and 'gossip, False)
---
+++ CodeWars/7kyu/strings/spacify.py
@@ -0,0 +1,6 @@
+# Return string with space between each character 
+
+def spacify(sentence):
+  return ' '.join(sentence)
+
+print(spacify("howdy"))
