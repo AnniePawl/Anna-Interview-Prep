596a2a471733aa5a3e5f8399c5e1f5dce8a2f78f
Anna Pawl <annampawl@gmail.com>
2020-06-24 23:14:37 -0400

    palindrome

--- CTI/replit_assignments/palindrome.py
+++ CTI/replit_assignments/palindrome.py
@@ -1,23 +1,17 @@
 # If string is palindrome, return 1, else return 0 
 
 def palindrome(text):
-  text = "".join(text)
-  return text
-  # left_index = 0
-  # right_index = len(text)-1
-  # # Ensure middle hasn't been surpased
-  # while left_index <= right_index:
-  #     if text[left_index] != text[right_index]:
-  #         return 0
-  #     else:
-  #         left_index += 1
-  #         right_index -= 1
-  # return 1
-
-
+# Remove punctuation and make lowercase 
+  text = text.lower()
+  punctuations = """?.",/: '"""
+  for char in text:
+    if char in punctuations:
+      text = text.replace(char, '')
+  
+  return 1 if text == text[::-1] else 0
   
   
-print(palindrome("anba")) # 0
+print(palindrome("anbA")) # 0
 print(palindrome("otto")) # 1
 print(palindrome("race car")) # 1
 print(palindrome("grub")) # 0
