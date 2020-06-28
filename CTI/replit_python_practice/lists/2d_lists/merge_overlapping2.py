63decf79e0b94480146ed0c720af61f16f68468e
Anna Pawl <annampawl@gmail.com>
2020-06-28 18:35:46 -0400

    merge overlapping intervals practice2

--- CTI/replit_python_practice/lists/2d_lists/merge_overlapping2.py
+++ CTI/replit_python_practice/lists/2d_lists/merge_overlapping2.py
@@ -1,26 +1,18 @@
 # Given three intervals, merge overlapping intervals 
 # Assume intervals are sorted
-import merge_overlapping2
 
-def merge_overlapping3(intervals):
-  for i in range(len(intervals)):
-    
-
-
-# def merge_overlapping2(intervals):
-#   x1 = intervals[0][0]
-#   x2 = intervals[1][0]
-#   y1 = intervals[0][1]
-#   y2 = intervals[1][1]
+def merge_overlapping2(intervals):
+  new_intervals = [intervals[0]]
+  for i in range(1, len(intervals)):
+    if new_intervals[-1][1] >= intervals[i][0]:
+      new_intervals[-1] = [new_intervals[-1][0], intervals[i][1]]
+    else:
+      new_intervals.append(intervals[i])
+  return new_intervals
+ 
   
-#   result = []
-#   if y1 >= x2:
-#     result.append([x1,y2])
-#   else:
-#     return intervals
-#   return result
+  
 
-
-
-print(merge_overlappying([[1,3],[2,6],[8,10]])) #[[1,6], [8,10]]
-print(merge_overlappying([[1,3],[2,6],[5,10]])) #[[1,10]]
+print(merge_overlapping2([[1,3],[2,6],[8,10]])) #[[1,6], [8,10]]
+print(merge_overlapping2([[1,3],[2,6],[5,10]])) #[[1,10]
+print(merge_overlapping2([[1,3],[2,6],[5,10],[7,15],[20,21]])) #[[1,15],[20,21]]
