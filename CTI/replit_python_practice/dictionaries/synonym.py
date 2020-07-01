f198a80149dbb9d110a492f17951fa997e4d882f
Anna Pawl <annampawl@gmail.com>
2020-07-01 16:04:28 -0400

    dictionary synonyms

--- CTI/replit_python_practice/dictionaries/synonym.py
+++ CTI/replit_python_practice/dictionaries/synonym.py
@@ -5,33 +5,55 @@ def synonym(word_dict, word):
 
 word_dict = {"Hello":"Hi", "Bye":"Goodbye", "List":"Array"}
 word = "Bye"
-
 print(synonym(word_dict, word))
 
 # Turn input into dictionay and do same as above 
+# My brute force sulution that took forever 
+a = int(input()) # number of entries (key,value pairs)
+my_dict = {}
+data = []
+for i in range(a):
+  data.append(input().split())
+for item in data:
+  my_dict[item[0]] = item[1]
+# print(my_dict)
 
-def synonym2():
-  dict = {}
-  i = 2
-  s = [s for s in input().split(" ")]
-  for word in s[:-1:2]:
-    dict[word] = s[i]
-    i+=2
-  return dict
+target = input()
 
-print(synonym2())
+if target in my_dict.keys():
+  print(my_dict[target])
+else:
+  for key, value in my_dict.items():
+    if value == target:
+      print(key)
+
+
+# CTI SOLUTION 
+# This strategy makes every input value a key AND a value
+synonyms = {}
+for i in range(int(input())):
+  w1,w2 = input().split()
+  synonyms[w1] = w2
+  synonyms[w2] = w1
+print(synonyms[input()])
 
 
 # Turn input into dictionay and do same as above 
-s = input()
-number_of_pairs = s[0]
-target = s[-1]
-my_dict = dict(s[1],s[-1])
-print(my_dict)
+# My brute force sulution that took forever 
+a = int(input()) # number of entries (key,value pairs)
+my_dict = {}
+data = []
+for i in range(a):
+  data.append(input().split())
+for item in data:
+  my_dict[item[0]] = item[1]
+# print(my_dict)
 
-# Sample Input 
-# 3
-# Hello Hi
-# Bye Goodbye
-# List Array
-# Goodbye
+target = input()
+
+if target in my_dict.keys():
+  print(my_dict[target])
+else:
+  for key, value in my_dict.items():
+    if value == target:
+      print(key)
