0d03f3bf936b2b3b49ea6a37c943ac2dafb4cafd
Anna Pawl <annampawl@gmail.com>
2020-07-02 16:06:50 -0400

    pascals triangle

--- CTI/replit_assignments/pascals_triangle.py
+++ CTI/replit_assignments/pascals_triangle.py
@@ -2,16 +2,20 @@
 # To generaate A[C] in row R, sum up A[C] and A[C-1] from previous row R-1
 # Since it's an interative problem, use a for loop
 
-# def pascals_triangle(numRows):
-  # triangle = []
-  # for i in range(1,numRows+1):
-  #   for j in range(i+1):
-  #     triangle.append(j)
-  #     print(triangle, end = "\n")
+def pascals_triangle(numRows):
+  triangle = [[] for i in range(numRows)]
+  for row in range(numRows):
+    for column in range(row + 1):
+      if column < row:
+        if column == 0:
+          triangle[row].append(1)
+        else:
+          triangle[row].append(triangle[row-1][column]+triangle[row-1][column-1])
+      elif (row == column):
+        triangle[row].append(1)
+  return triangle
 
-
-
-# print(pascals_triangle(5))
+print(pascals_triangle(5))
 # [
 #      [1],
 #      [1,1],
@@ -20,31 +24,19 @@
 #      [1,4,6,4,1]
 # ]
 
-# rows = 8;
-# triangle = [];
 
-# for i in range(rows):
-#   row = [];
-#   row.append(1);
-#   for j in range(i):
-#       row.append(2+j);
-#   triangle.append(row);
-    
-# print(triangle)
+# Another Solution 
+def pascal_triangle(numRows):
+  current_row=[1]
+  triangle = [current_row]
+  for i in range(1,numRows):
+      next_row = []
+      next_row.append(current_row[0])
+      for i in range(len(current_row)-1):
+          next_row.append(current_row[i]+current_row[i+1])
+      next_row.append(current_row[-1])
+      current_row=next_row
+      triangle.append(next_row)
+  return triangle
 
-# Stepping Stones 
-n = 5 
-print ("1" * n,  end = ' ')
-print (str(1) * n, end = ' ')
-
-
-int_list = [1,3,3,1]
-result = [1,4,6,4,1]
-
-for i in range(len(int_list)):
-  
-
-
-# Given a list of ints , print row w/ len(int) + 1
-# First and last nume printed will be 1 
-# ith num will be sum of value of ith value one 
+print(pascal_triangle(5))
---
+++ CTI/replit_assignments/rotate_matrix.py
@@ -0,0 +1,17 @@
+# Given NxN matrix, rotate it by 90 degrees 
+# Perform operations in place using O(1) space 
+
+def rotate_matrix(matrix):
+
+
+print(rotate_matrix([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]))
+
+#  [
+#   [7, 4, 1]
+#   [8, 5, 2]
+#   [9, 6, 3]
+# ]
