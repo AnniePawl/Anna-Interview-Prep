c8b60930d1fcdd6331ed6f3f556de1ef6d561dd1
Anna Pawl <annampawl@gmail.com>
2020-07-09 11:50:39 -0400

    lost card

--- CTI/replit_python_practice/for_loops/lost_card.py
+++ CTI/replit_python_practice/for_loops/lost_card.py
@@ -1,12 +1,22 @@
 # Given set of cards from 1 to N, return card that's missing. 
 # N is followed by N-1 integers 
 
+
+# Set strategy 
 a = int(input())
-cards = []
+all_cards  = set(x for x in range(1,a+1))
+cards = set()
 for i in range(a-1):
-  cards.append(int(input()))
-cards.sort()
+  cards.add(int(input()))
 
-print(x for x in range(cards[0], cards[-1]+1) if x not in cards)
+print(all_cards.difference(cards))
 
-print(sorted(set(range(cards[0], cards[-1])) - set(cards)) )
+
+# Sum / subtraction strategy 
+n = int(input())
+cards_sum = 0 
+for i in rannge(1,n +1):
+  cards_sum + i 
+for i in range(n-1):
+  cards_sum -= int(input()):
+print(cards_sum)
