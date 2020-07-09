d4ac2bb42799e53d7a36d1cba39ad4fc33748c4c
Anna Pawl <annampawl@gmail.com>
2020-07-08 21:46:13 -0400

    bubble sort and merge sort in progress

---
+++ CTI/replit_assignments/merge_sort.py
@@ -0,0 +1,9 @@
+# Given an unsorted list of integers, sort those integers using the Bubble Sort algorithm. Return the number of swaps required to sort the list.
+
+# Bubble Sort is a simple sorting algorithm that works as follows:
+# 1. Start at the beginning of the list, and traverse through each element.
+# 2. For each element, if the element is larger than the one that comes after it, swap them.
+# 3. Once you reach the end of the list, if you have not made any swaps, you are done, and the list is sorted.
+# 4. Otherwise, go back to step 1.
+
+# Given a list of numbers to sort, return the count of swaps you must make when using the Bubble Sort Algorithm that will result in a sorted list.
