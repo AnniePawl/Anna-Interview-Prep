180ce526cdfccbc79483dbb635620aee792d32e8
Anna Pawl <annampawl@gmail.com>
2020-07-12 17:22:40 -0400

    min chars for insertion

--- CTI/replit_assignments/min_chars_for_palindrome.py
+++ CTI/replit_assignments/min_chars_for_palindrome.py
@@ -1,15 +1,40 @@
 # Given a string, only operation allowed is insert chars at beginning of string 
 # Return num of chars needed to make string a palindrom 
 
-def min_chars(chars):
-  count = 0
-  if chars == chars[::-1]:
-    return count
-  else:
-    chars
+# Strategy - remove letters from string until empty or palindrome 
+def min_chars_del(chars):
+  chars = list(chars)
+  count = 0 
+  while len(chars) > 0 :
+    if chars == chars[::-1]:
+      return count
+    else:
+      chars.pop()
+      count +=1
+
+def is_palindrome(chars):
+  return chars == chars[::-1]
+
+def min_chars_insert(chars):
+  insertions = 0
+  while not is_palindrome(chars) and len(chars) >1:
+    chars = chars[0:-1]
+    insertions +=1
+  return insertions
+ 
 
 
-print(min_chars("ABA")) # 0
-print(min_chars("AB")) # 1
-# print(min_chars("ABC")) # 2
-# print(min_chars("AACECAAAA")) # 2
+# Del Approach
+print(min_chars_del("ABA")) # 0
+print(min_chars_del("AB")) # 1
+print(min_chars_del("ABC")) # 2
+print(min_chars_del("AACECAAAA")) # 2
+
+# Insert Approach 
+print("insert approach")
+print(min_chars_insert("ABA")) # 0
+print(min_chars_insert("AB")) # 1
+print(min_chars_insert("ABC")) # 2
+print(min_chars_insert("AACECAAAA")) # 2
+
+
