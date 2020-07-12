de55af8f6f9bbaa8e0f0eabb06dc7a0dc57e8d5f
Anna Pawl <annampawl@gmail.com>
2020-07-12 12:41:51 -0400

    k most frequent elements

--- SPD1.41/k_most_frequent.py
+++ SPD1.41/k_most_frequent.py
@@ -7,6 +7,25 @@ def k_most_frequent(s, k):
       word_count[word] +=1
     else:
       word_count[word] = 1
+  
+  k_most_frequent = []
+  while k > 0:
+    most_frequent = (max(word_count, key = word_count.get))
+    k_most_frequent.append(most_frequent)
+    word_count.pop(most_frequent)
+    k -= 1
 
-  return word_count
-print(k_most_frequent("the sun is here the sun is there the sun is everywhere", 2))
+  return k_most_frequent
+
+print(k_most_frequent("the sun is here the sun is there the sun is everywhere", 3)) # ["the", "sun", "is"]
+
+
+# Strategy One: Using Counter
+# Time Complexity: O(n)
+from collections import Counter 
+def counter(s):
+  words_list = s.split()
+  return Counter(words_list).most_common(2)
+
+print(counter("brown dog brown cat brown hen brown dog"))
+
